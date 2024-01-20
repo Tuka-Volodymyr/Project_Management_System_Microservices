bff1d14376d5e19a0872a4aef94684751fbdeb6c
TukaVolodymyr <tukavolody@gmail.com>
2024-01-20 17:36:53 +0200

    Add EnableEurekaServer

---
+++ server_registry/src/main/java/com/example/server_registry/ServerRegistryApplication.java
@@ -0,0 +1,17 @@
+package com.example.server_registry;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+@Slf4j
+public class ServerRegistryApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ServerRegistryApplication.class, args);
+  }
+
+}
