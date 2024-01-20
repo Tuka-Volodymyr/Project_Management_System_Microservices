633f15bb6ad17cbb9c55e24f19504304c94238b0
TukaVolodymyr <tukavolody@gmail.com>
2024-01-20 17:40:31 +0200

    Add Gateway service

---
+++ gateway_service/src/main/java/com/example/gateway_service/util/JwtUtil.java
@@ -0,0 +1,34 @@
+package com.example.gateway_service.util;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import java.util.Date;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class JwtUtil {
+
+
+  @Value("${SECRET_KEY}")
+  private String secretKey;
+
+
+  public void validateToken(final String token) {
+    Jwts.parserBuilder().setSigningKey(getSignKey()).build().parseClaimsJws(token);
+  }
+
+
+  private Key getSignKey() {
+    byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+    return Keys.hmacShaKeyFor(keyBytes);
+  }
+
+}
+
